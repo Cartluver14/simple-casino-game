e029f4f04ff2db67afca07ec3241ae4406131c47
North Harrington <339713067@GoTVDSB.ca>
2024-10-08 09:35:54 -0400

    Add project files.

---
+++ simple casino game/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace simple_casino_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int result;
+            int score = 3;
+            int tails = 0;
+            int heads = 0;
+            string answer;
+
+
+           
+            
+
+            while (score >= 0)
+            {
+                Console.WriteLine("heads or tails?");
+                answer = Console.ReadLine().ToLower();
+                result = rnd.Next(2);
+                if (result == 1 && answer == "tails")
+                    Console.WriteLine("Correct");
+                {
+                    Console.WriteLine("Tails");
+                    tails = tails + 1;
+                    score = score + 1;
+
+
+                }
+                else if (result == 0)
+                {
+
+                    Console.WriteLine("Heads");
+                    heads = heads + 1;
+                    score = score + 1;
+                }
+
+
+
+
+            }
+
+
+            
+            
+            Console.WriteLine("Hello welcome to my casino game ");
+
+        }
+    }
+}
