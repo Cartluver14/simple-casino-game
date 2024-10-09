5dc87be97f849044e306cf61099e6351d8564da8
North Harrington <339713067@GoTVDSB.ca>
2024-10-09 09:25:57 -0400

    not done yet

--- simple casino game/Program.cs
+++ simple casino game/Program.cs
@@ -17,25 +17,51 @@
 
             while (score >= 0)
             {
+                Console.WriteLine("Hello welcome to my casino game ");
                 Console.WriteLine("heads or tails?");
                 answer = Console.ReadLine().ToLower();
                 result = rnd.Next(2);
                 if (result == 1 && answer == "tails")
-                    Console.WriteLine("Correct");
+                    
                 {
                     Console.WriteLine("Tails");
+                    Console.WriteLine("Correct");                    
                     tails = tails + 1;
                     score = score + 1;
+                    Console.WriteLine(" Your score is "  +score);
 
 
                 }
-                else if (result == 0)
-                {
-
+                 else if (result == 0 && answer == "heads")
+                {                   
                     Console.WriteLine("Heads");
+                    Console.WriteLine("correct");
                     heads = heads + 1;
                     score = score + 1;
+                    Console.WriteLine(" Your score is "   + score);
                 }
+                if (result == 0 && answer == "tails")
+                {
+
+                    Console.WriteLine("Heads!");
+                    Console.WriteLine("Incorrect, try again");
+                    
+                    score = score - 1;
+                    Console.WriteLine(" Your score is "  +   score);
+
+                }
+                if (result == 1 && answer == "heads")
+                {
+
+                    Console.WriteLine("Tails" +
+                        "!");
+                    Console.WriteLine("Incorrect, try again");
+
+                    score = score - 1;
+                    Console.WriteLine(" Your score is " +   score);
+
+                }
+
 
 
 
@@ -43,9 +69,9 @@
             }
 
 
+
+
             
-            
-            Console.WriteLine("Hello welcome to my casino game ");
 
         }
     }
